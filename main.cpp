56ae7c76285b101feaf4c1d789b906929b738cea
LV-ZHU <2452487@tongji.edu.cn>
2026-08-11 16:46:31 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -29,16 +29,38 @@ int main()
 	station_a.name = "Tongji University";
 	station_a.type = station_type::METRO;
 	
+	vertex station_b;
+	station_b.id = 1;
+	station_b.name = "Siping Road";
+	station_b.type = station_type::METRO;
+
 	edge_node edge_a_to_b;
 	edge_a_to_b.to = 1;
 	edge_a_to_b.time_cost = 3;
 	edge_a_to_b.fare_cost = 0.3;
 	edge_a_to_b.type = edge_type::METRO;
 	station_a.first_edge = &edge_a_to_b;
+
+	edge_node edge_a_to_c;
+	edge_a_to_c.to = 2;
+	edge_a_to_c.time_cost = 6;
+	edge_a_to_c.fare_cost = 0;//换乘边不额外收费
+	edge_a_to_c.type = edge_type::TRANSFER;
+	edge_a_to_c.next = station_a.first_edge;//头插法
+	station_a.first_edge = &edge_a_to_c;
+	
+
+
 	if (station_a.first_edge == nullptr)
 		cout << "该节点为孤立顶点，当前站点暂无可用路线" << endl;
-	else
-		cout << station_a.first_edge->to;//这里语法有点绕，通过指针访问它所指对象的成员使用箭头有点晕，是相当于(*station_a.first_edge).to的语法糖是吧
+	else {
+		edge_node* current_edge = station_a.first_edge;//指针从站点第一个节点开始
+		while (current_edge) {
+			cout << current_edge->to << " ";
+			current_edge = current_edge->next;
+		}
+	}
+		
 
 
 	return 0;
